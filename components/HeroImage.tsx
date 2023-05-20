aaf451247e7b40cbf05c7bad5f47c9c8c4302e36
Benjamin Arko Afrasah <barkoafrasah@gmail.com>
2023-05-20 22:18:25 +0000

    fix: temp solution for FCL hero image

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 
 const HeroImage = () => {
-	return <Image src="/images/hero/img-hero.webp" alt="landing_cup" width={416} height={416} fetchPriority='auto' />;
+	return <Image src="/images/hero/img-hero.webp" alt="landing_cup" width={416} height={416} priority/>;
 };
 
 export default HeroImage;
