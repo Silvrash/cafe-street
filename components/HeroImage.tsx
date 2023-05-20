bd4c2042122c80c65dbf59b7fa84a559cf3f0280
Benjamin Arko Afrasah <barkoafrasah@gmail.com>
2023-05-20 22:15:26 +0000

    feat: some extra site optimizations to Defer offscreen images

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -1,20 +1,9 @@
 'use client';
 
 import Image from 'next/image';
-import { useRef } from 'react';
 
-const lg = '(min-width: 1024px)';
 const HeroImage = () => {
-	const priority = useRef(window.matchMedia(lg).matches);
-	return (
-		<Image
-			src="/images/hero/img-hero.webp"
-			alt="landing_cup"
-			width={416}
-			height={416}
-			priority={priority.current}
-		/>
-	);
+	return <Image src="/images/hero/img-hero.webp" alt="landing_cup" width={416} height={416} fetchPriority='auto' />;
 };
 
 export default HeroImage;
